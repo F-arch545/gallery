b6301f79ddb6a922eb3c9e27d34c7d2b93a7406d
flozy irungu <flozyirungu545@gmail.com>
2026-01-15 01:42:43 +0300

    done with milestone 3

--- _config.js
+++ _config.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
   dbUrl: 'mongodb+srv://flozy:flozy@123@cluster1.ksklg53.mongodb.net/?appName=Cluster1'
 };
-=======
-var config = {}
-
-// Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
-module.exports = config;
->>>>>>> test
