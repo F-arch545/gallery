62add85b2321a5bb9a9f53aa3a28af4b683cfe02
flozy irungu <flozyirungu545@gmail.com>
2026-01-19 15:25:46 +0300

    updated files

--- test/serverTest.js
+++ test/serverTest.js
@@ -2,20 +2,26 @@ process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
 const app = require('../server');
 
 chai.use(chaiHttp);
-
 const should = chai.should();
 
 describe('Photos', function () {
-  it('should list ALL photos on / GET', function (done) {
-    this.timeout(60000);
+  // Keep Mocha timeout, but also enforce request timeout
+  this.timeout(20000);
 
+  it('should respond on / GET', function (done) {
     chai.request(app)
       .get('/')
+      .timeout({ response: 10000, deadline: 15000 }) // 👈 prevents hanging forever
       .end(function (err, res) {
+        if (err) {
+          // show the actual cause instead of a dumb timeout
+          console.error('Request error:', err.message);
+          return done(err);
+        }
+
         res.should.have.status(200);
         res.should.be.html;
         res.text.should.be.a('string');
